7d63601179f88e5c725611d652ed030fa6aa76f2
Megan Reardon <meganr@gmail.com>
2017-03-07 13:01:58 -0800

    added post test for auth route

---
+++ test/auth-route-test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../model/user.js');
+
+mongoose.Promise = Promise;
+
+require('../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'Example User',
+  password: '1234',
+  email: 'exampleuser@test.com'
+};
+
+describe('Auth Routes', function() {
+  describe('POST: /api/signup', function() {
+    describe('with a valid body', function() {
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) =>  {
+          if (err) return done(err);
+          console.log('\n ::: res.text is: ', res.text, '\n');
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe
